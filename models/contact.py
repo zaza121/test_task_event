02402516e4986b04993973ff61a7c12a58658238
mr zaza <hamza.yaya121@gmail.com>
2021-01-27 18:57:24 +0100

    [ADD] Add code to sanitize number before save it
    Note : All phones are stored as 10 digits - no brackets, spaces or dashes

--- models/contact.py
+++ models/contact.py
@@ -13,6 +13,22 @@ class Contact:
             print(name)
             raise ValueError(NAME_VALUE_NULL)
 
+        # clean number
+        if phone:
+            phone = str(phone)
+            phone = phone.replace('(', '')
+            phone = phone.replace(')', '')
+            phone = phone.replace('-', '')
+            phone = phone.replace(' ', '')
+
+            if len(phone) > 10:
+                raise ValueError("10 digits only for numbers")
+
+            try:
+                phone = int(phone)
+            except:
+                raise ValueError("Number format not correct")
+
         self.name = name
         self.email = email
         self.phone = phone
--- models/lead.py
+++ models/lead.py
@@ -7,6 +7,23 @@ class Lead:
     phone = ""
 
     def __init__(self, name="", email="", phone=""):
+
+        # clean number
+        if phone:
+            phone = str(phone)
+            phone = phone.replace('(', '')
+            phone = phone.replace(')', '')
+            phone = phone.replace('-', '')
+            phone = phone.replace(' ', '')
+
+            if len(phone) > 10:
+                raise ValueError("10 digits only for numbers")
+
+            try:
+                phone = int(phone)
+            except:
+                raise ValueError("Number format not correct")
+
         self.name = name
         self.email = email
         self.phone = phone
